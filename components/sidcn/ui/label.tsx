84a2c72e37d62946277813fabe0494724efe2e88
sidghimire <siddharthaghimire@gmail.com>
2024-02-17 14:59:38 +0545

    Update

--- components/sidcn/ui/label.tsx
+++ components/sidcn/ui/label.tsx
@@ -1,21 +1,29 @@
-import React from "react";
-import { Text } from "react-native";
-import { twMerge } from "tailwind-merge";
-import { separateTextClassNames, classifyChild } from "../utils/textFunction";
+import React, { ReactNode } from 'react';
+import { Text, TextProps } from 'react-native';
+import { twMerge } from 'tailwind-merge';
+import { classifyChild } from '../utils/textFunction';
 
-const Label = ({ children, className = "", ...props }) => {
+interface LabelProps extends TextProps {
+  children: ReactNode;
+  className?: string;
+}
+
+const Label: React.FC<LabelProps> = ({ children, className = '', ...props }) => {
   const childType = classifyChild(children);
 
-  if (childType === "text") {
+  if (childType === 'text') {
     return (
-      <Text className={twMerge("text-sm m-1 text-gray-900 dark:text-gray-100", className)} {...props}>
+      <Text
+        className={twMerge('text-sm m-1 text-gray-900 dark:text-gray-100', className)}
+        {...props}
+      >
         {children}
       </Text>
     );
   }
 
   // If it's a component, return it as is
-  return children;
+  return children as React.ReactElement;
 };
 
 export default Label;
